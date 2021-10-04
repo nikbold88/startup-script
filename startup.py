7cf01aaea645107916693f8481e4d653209f92b3
nikbold88 <90900186+nikbold88@users.noreply.github.com>
2021-10-04 08:08:11 +0100

    Chrome working and outlook working

--- startup.py
+++ startup.py
@@ -3,21 +3,19 @@
 #### What Script has to do:
 
 ## VPN
-Connect to Dev VPN
+# Connect to Dev VPN
 
 ## Chrome
+import webbrowser
 
-# Open GIT:
-https://github.com/nikbold/
-
-# GLPI
-http://192.168.11.10/glpi/
-
-# Open CI Team Board:
-https://git.dev.zynstra.com/users/sign_in # 2fa????
-
-# Open Rundeck:
-https://rundeck.dev.zynstra.com/project/deployments/activity
+url = 'https://github.com/nikbold/' # Open GIT:
+url1 = 'http://192.168.11.10/glpi/' # GLPI
+url2 = 'https://github.com/nikbold?tab=repositories' # Open CI Team Board:
+url3 = 'https://rundeck.dev.zynstra.com/project/deployments/activity' # Open Rundeck:
+webbrowser.open_new(url)
+webbrowser.open_new(url1)
+webbrowser.open_new(url2)
+webbrowser.open_new(url3)
 
 ## WSL2
 
@@ -28,3 +26,4 @@ https://rundeck.dev.zynstra.com/project/deployments/activity
 # Open Outlook
 import os
 os.startfile("outlook")
+
