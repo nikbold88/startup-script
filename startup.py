53d664febe3cb19e969db39074aa2d351a570a60
nikbold <nik.bold@zynstra.com>
2021-10-04 07:49:36 +0100

    Create startup.py

---
+++ startup.py
@@ -0,0 +1,30 @@
+# Nik's Startup Script
+
+#### What Script has to do:
+
+## VPN
+Connect to Dev VPN
+
+## Chrome
+
+# Open GIT:
+https://github.com/nikbold/
+
+# GLPI
+http://192.168.11.10/glpi/
+
+# Open CI Team Board:
+https://git.dev.zynstra.com/users/sign_in # 2fa????
+
+# Open Rundeck:
+https://rundeck.dev.zynstra.com/project/deployments/activity
+
+## WSL2
+
+# Open New Window
+
+## Outlook
+# Scrape new emails
+# Open Outlook
+import os
+os.startfile("outlook")
